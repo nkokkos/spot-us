23eb6be8e696af45c635f65ab3da492352fd72bf
Nick Kokkos <nkokkos@gmail.com>
2010-12-27 15:26:34 +0200

    commented Network.create! in LoadDisplayNameForDefaultNetwork

--- db/migrate/20090219203016_load_display_name_for_default_network.rb
+++ db/migrate/20090219203016_load_display_name_for_default_network.rb
@@ -1,7 +1,7 @@
 class LoadDisplayNameForDefaultNetwork < ActiveRecord::Migration
   def self.up
     #Network.reset_column_information
-    Network.create!(:name => 'sfbay', :display_name => 'Bay Area')
+  #  Network.create!(:name => 'sfbay', :display_name => 'Bay Area')
   end
 
   def self.down
